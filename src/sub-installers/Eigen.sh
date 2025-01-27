6fdb7e4a7427780c0f91893da25783f5c9a9c551
みどりむし <64454054+uni-kakurenbo@users.noreply.github.com>
2025-01-27 23:37:34 +0000

    fixup! pkg-config

--- src/sub-installers/Eigen.sh
+++ src/sub-installers/Eigen.sh
@@ -3,9 +3,7 @@ set -eu
 
 sudo apt-get install -y "libeigen3-dev=${VERSION}"
 
-sudo mkdir -p /opt/ac_install/eigen3/include/Eigen/
-sudo mkdir -p /opt/ac_install/eigen3/include/unsupported/Eigen/CXX11
+sudo mkdir -p /opt/ac_install/eigen3/include/
 
-find /usr/include/eigen3/Eigen/ -maxdepth 1 -type f -exec bash -c 'sudo cp "$1" /opt/ac_install/eigen3/include/Eigen/' -- {} \;
-find /usr/include/eigen3/unsupported/Eigen/ -maxdepth 1 -type f -exec bash -c 'sudo cp "$1" /opt/ac_install/eigen3/include/unsupported/Eigen/' -- {} \;
-find /usr/include/eigen3/unsupported/Eigen/CXX11 -maxdepth 1 -type f -exec bash -c 'sudo cp "$1" /opt/ac_install/eigen3/include/unsupported/Eigen/CXX11/' -- {} \;
+sudo cp -Trf /usr/include/eigen3/ /opt/ac_install/eigen3/include/
+sudo rm -rf /usr/include/eigen3/
